2ee051faf424a767ca5cb67f0d3fc0c37610ba41
Dr. Dillinger <peter.dillinger@srh.de>
2023-11-08 10:55:33 +0100

    Berechnung der Quersumme mit Hilfe der ganzzahligen Division und der Modulo (%) Berechnung.

--- src/Quersumme.java
+++ src/Quersumme.java
@@ -6,6 +6,15 @@ public class Quersumme {
         System.out.print("Bitte Zahl eingeben: ");
         int zahl = eingabe.nextInt();
 
-        // TODO
+        System.out.print("Die Quersumme der Zahl " + zahl + " = ");
+        int summe = 0;
+        while ( zahl > 0 ) {
+            int ziffer = zahl % 10;
+            System.out.print(ziffer);
+            summe = summe + ziffer;
+            zahl = zahl / 10;
+            if (zahl > 0) System.out.print(" + ");
+        }
+        System.out.println(" = " + summe);
     }
 }
